23cd348fb2ef58955f5cd72ebe8afdfe5bf65bb3
ImDaveead <imdaveead@gmail.com>
2017-09-26 16:15:58 -0400

    ShopHandler

--- src/wearedevs/javaclicker/shop/items/perClickUpgrades/PerClick2.java
+++ src/wearedevs/javaclicker/shop/items/perClickUpgrades/PerClick2.java
@@ -1,12 +1,14 @@
 package wearedevs.javaclicker.shop.items.perClickUpgrades;
 
 import wearedevs.javaclicker.Main;
+import wearedevs.javaclicker.shop.ShopHandler;
 import wearedevs.javaclicker.shop.ShopItem;
 
 public class PerClick2 extends ShopItem {
 
 	public void onPurchase() {
 		Main.perClick = 2;
+		ShopHandler.unlock(new PerClick5());
 	}
 
 	public int getPrice() {
