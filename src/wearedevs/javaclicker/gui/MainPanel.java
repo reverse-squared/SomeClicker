ad56f061021b9ca77723f2b46d67fec6afd48707
ImDaveead <imdaveead@gmail.com>
2017-09-25 20:29:32 -0400

    Changes
    Fixed mainPanel
    Moved button + label

--- src/wearedevs/javaclicker/gui/MainPanel.java
+++ src/wearedevs/javaclicker/gui/MainPanel.java
@@ -1,6 +1,14 @@
 package wearedevs.javaclicker.gui;
 
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import wearedevs.javaclicker.Main;
 
 public class MainPanel extends JPanel {
 	
@@ -8,6 +16,24 @@ public class MainPanel extends JPanel {
 		setLayout(null);
 		setBounds(100, 100, 640, 480);
 		
+		
+		JLabel lblClicks = new JLabel("Clicks: " + Main.clicks);
+		lblClicks.setFont(new Font("Tahoma", Font.BOLD, 25));
+		lblClicks.setBounds(12, 13, 226, 34);
+		add(lblClicks);
+		
+		JButton clicker = new JButton("Click Me!");
+		clicker.setFont(new Font("Tahoma", Font.BOLD, 20));
+		clicker.addActionListener(new ActionListener() {
+			int clickCount = 0;
+			
+			public void actionPerformed(ActionEvent arg0) {
+				clickCount += Main.perClick;
+				lblClicks.setText("Clicks: " + clickCount);
+			}
+		});
+		clicker.setBounds(12, 58, 610, 100);
+		add(clicker);
 	}
 
 }
