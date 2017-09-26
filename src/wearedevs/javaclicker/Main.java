16d4352fa7d36e580b5768de51c86e87402737cc
EXPGamer303 <expgamer303@gmail.com>
2017-09-26 16:00:31 -0400

    Made panelSize and windowSize Two Diffrent Variables

--- src/wearedevs/javaclicker/Main.java
+++ src/wearedevs/javaclicker/Main.java
@@ -22,6 +22,7 @@ public class Main extends JFrame {
 	
 	public static final String VERSION = "DEV 0.1";
 	public static final Rectangle windowSize = new Rectangle(100, 100, 640, 480);
+	public static final Rectangle panelSize = new Rectangle(0, 0, 640, 480);
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Loading Java Clicker "+VERSION);
