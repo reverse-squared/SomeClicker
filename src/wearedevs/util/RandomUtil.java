ee8801ac033a898555186daffcd0e6d722b74f5e
ImDaveead <imdaveead@gmail.com>
2017-09-26 18:18:34 -0400

    Float and Long Random Range

--- src/wearedevs/util/RandomUtil.java
+++ src/wearedevs/util/RandomUtil.java
@@ -4,7 +4,12 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomUtil {
 	public static int randomRange(int low, int high) {
-		return ThreadLocalRandom.current().nextInt(low, high + 1);
-		
+		return ThreadLocalRandom.current().nextInt(low, high + 1);	
+	}
+	public static double randomRange(double low, double high) {
+		return ThreadLocalRandom.current().nextDouble(low, high + 1);
+	}
+	public static long randomRange(long low, long high) {
+		return ThreadLocalRandom.current().nextLong(low, high + 1);
 	}
 }
