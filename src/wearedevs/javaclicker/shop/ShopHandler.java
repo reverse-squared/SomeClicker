c8302ee458a4a91bec17504cb54a0f4311959301
ImDaveead <imdaveead@gmail.com>
2017-09-26 16:15:58 -0400

    ShopHandler

--- src/wearedevs/javaclicker/shop/ShopHandler.java
+++ src/wearedevs/javaclicker/shop/ShopHandler.java
@@ -1,5 +1,21 @@
 package wearedevs.javaclicker.shop;
 
+import java.util.ArrayList;
+
+import wearedevs.javaclicker.shop.items.perClickUpgrades.PerClick2;
+
 public class ShopHandler {
 
+	public static ArrayList<ShopItem> items;
+	/**
+	 * @author ImDaveead
+	 */
+	public static void initializeShop() {
+		//Insert All Things By Default;
+		unlock(new PerClick2());
+		
+	}
+	public static void unlock(ShopItem item) {
+		items.add(item);
+	}
 }
