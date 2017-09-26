2247d0a9ecc81139506551d455d6d3f43cc165ec
hparcells <hunterparcells@gmail.com>
2017-09-26 15:44:25 -0400

    Added More Upgrades

---
+++ src/wearedevs/javaclicker/shop/items/autoClickers/Auto1.java
@@ -0,0 +1,21 @@
+package wearedevs.javaclicker.shop.items.autoClickers;
+
+import wearedevs.javaclicker.Main;
+import wearedevs.javaclicker.shop.ShopItem;
+
+public class Auto1 extends ShopItem {
+
+	public void onPurchase() {
+		//Do Stuff
+	}
+
+	public int getPrice() {
+		return 700;
+	}
+
+	public String getName() {
+		//5 Clicks a Second
+		return "Auto Clicker: Tier 1";
+	}
+
+}
---
+++ src/wearedevs/javaclicker/shop/items/autoClickers/Auto2.java
@@ -0,0 +1,21 @@
+package wearedevs.javaclicker.shop.items.autoClickers;
+
+import wearedevs.javaclicker.Main;
+import wearedevs.javaclicker.shop.ShopItem;
+
+public class Auto2 extends ShopItem {
+
+	public void onPurchase() {
+		//Do Stuff
+	}
+
+	public int getPrice() {
+		return 1500;
+	}
+
+	public String getName() {
+		//10 Clicks a Second
+		return "Auto Clicker: Tier 1";
+	}
+
+}
