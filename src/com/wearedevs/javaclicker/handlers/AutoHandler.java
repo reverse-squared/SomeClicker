9bc4b379cf2bb6cdf996f59aefd11865bc765383
EXPGamer303 <expgamer303@gmail.com>
2017-09-27 13:43:28 -0400

    Added Click Mine

--- src/com/wearedevs/javaclicker/handlers/AutoHandler.java
+++ src/com/wearedevs/javaclicker/handlers/AutoHandler.java
@@ -17,8 +17,12 @@ public class AutoHandler {
 	public static boolean autoClickStarted = false;
 	
 	public static void initAutoThread() {
-		if(autoClickStarted) return;
+		if(autoClickStarted) {
+			return;
+		}
+		
 		autoClickStarted = true;
+		
 		autoThread = new Thread(new Runnable() {
 			public void run() {
 				int bomb_count = 0;
@@ -26,13 +30,16 @@ public class AutoHandler {
 				try {
 					while (autoClickStarted) {
 						bomb_count++;
+						
 						if (bomb_count>100) {
 							bomb_count = 0;
 							double bomb = (clickBomb + RandomUtil.randomRange(clickBomb * -0.15, clickBomb * 0.15));
 							Main.clicks += bomb;
 						}
-						Main.clicks += autoClick/10d;
+						
+						Main.clicks += autoClick / 10d;
 						Main.updateCounter();
+						
 						//TODO: Better Timer
 						Thread.sleep(100L);
 					}
---
+++ src/com/wearedevs/javaclicker/shop/items/mines/Mine1.java
@@ -0,0 +1,21 @@
+package com.wearedevs.javaclicker.shop.items.mines;
+
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class Mine1 extends ShopItem{
+
+	//TODO: 2 Clicks a Second
+	
+	public void onPurchase() {
+		//TODO: Make Mine Cycle
+	}
+
+	public int getPrice() {
+		return 2500;
+	}
+
+	public String getName() {
+		return "Click Mine: Tier 1";
+	}
+
+}
