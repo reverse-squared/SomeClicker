0e0a013835214c48256a6f660619c7a0004292dc
EXPGamer303 <expgamer303@gmail.com>
2017-09-27 07:25:38 -0400

    Added Factory Upgrade

---
+++ src/com/wearedevs/javaclicker/shop/items/factories/Factory1.java
@@ -0,0 +1,20 @@
+package com.wearedevs.javaclicker.shop.items.factories;
+
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class Factory1 extends ShopItem {
+
+	//TODO: 10 Clicks Every 5 Seconds
+	public void onPurchase() {
+		//TODO: Add Factory Cycle
+		//TODO: Add Factory 2
+	}
+
+	public int getPrice() {
+		return 1000;
+	}
+
+	public String getName() {
+		return "Click Factory: Tier 1";
+	}
+}
