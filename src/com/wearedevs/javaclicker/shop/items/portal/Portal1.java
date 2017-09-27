d8a56b33a79dc42e9808959169cb34dc6ddcb2f4
hparcells <hunterparcells@gmail.com>
2017-09-27 14:02:34 -0400

    Fixed Small Mistake, Added Click Portal

---
+++ src/com/wearedevs/javaclicker/shop/items/portal/Portal1.java
@@ -0,0 +1,21 @@
+package com.wearedevs.javaclicker.shop.items.portal;
+
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class Portal1 extends ShopItem {
+
+	//TODO: 500 Clicks Every 15 Seconds
+	
+	public void onPurchase() {
+		//TODO: Make Mine Cycle
+	}
+
+	public int getPrice() {
+		return 25000;
+	}
+
+	public String getName() {
+		return "Click Portal: Tier 2";
+	}
+
+}
