cc5dd4c5bb0b0b207220b833c335151b150d6e39
hparcells <hunterparcells@gmail.com>
2017-09-27 07:25:38 -0400

    Added Factory Upgrade

--- src/com/wearedevs/javaclicker/shop/items/bombs/Bomb1.java
+++ src/com/wearedevs/javaclicker/shop/items/bombs/Bomb1.java
@@ -3,6 +3,7 @@ package com.wearedevs.javaclicker.shop.items.bombs;
 import com.wearedevs.javaclicker.handlers.AutoHandler;
 import com.wearedevs.javaclicker.handlers.ShopHandler;
 import com.wearedevs.javaclicker.shop.ShopItem;
+import com.wearedevs.javaclicker.shop.items.factories.Factory1;
 
 public class Bomb1 extends ShopItem {
 
@@ -10,6 +11,7 @@ public class Bomb1 extends ShopItem {
 		AutoHandler.clickBomb = 100;
 		
 		ShopHandler.unlock(new Bomb2());
+		ShopHandler.unlock(new Factory1());
 		
 		//Init AutoClicker
 		AutoHandler.initAutoThread();
