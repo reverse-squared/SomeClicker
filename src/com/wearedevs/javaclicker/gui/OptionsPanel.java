8421eef2491ce40b1c665719e617859d907bf4eb
ImDaveead <imdaveead@gmail.com>
2017-09-28 16:51:36 -0400

    commit

---
+++ src/com/wearedevs/javaclicker/gui/OptionsPanel.java
@@ -0,0 +1,18 @@
+package com.wearedevs.javaclicker.gui;
+
+import javax.swing.JPanel;
+
+import com.wearedevs.javaclicker.Main;
+
+@SuppressWarnings("serial")
+public class OptionsPanel extends JPanel {
+
+	/**
+	 * Create the frame.
+	 */
+	public OptionsPanel() {
+		setLayout(null);
+		setBounds(Main.windowSize);
+	}
+
+}
