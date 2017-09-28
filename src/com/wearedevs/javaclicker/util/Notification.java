f7bed412181a6d0eded62306df675ba43c29cc48
ImDaveead <imdaveead@gmail.com>
2017-09-28 17:11:16 -0400

    notifacation

--- src/com/wearedevs/javaclicker/util/Notification.java
+++ src/com/wearedevs/javaclicker/util/Notification.java
@@ -4,30 +4,36 @@ import java.awt.*;
 import java.awt.TrayIcon.MessageType;
 
 public class Notification {
-
-    public static void main(String[] args) throws AWTException, java.net.MalformedURLException {
+	public static TrayIcon trayIcon;
+	public static void init(String name, String tooltip, String iconPath) {
+        if (!SystemTray.isSupported()) {
+            System.err.println("System tray not supported!");
+            return;
+        }
+        SystemTray tray = SystemTray.getSystemTray();
+		Image image = Toolkit.getDefaultToolkit().createImage(ClassLoader.getSystemClassLoader().getResource(iconPath));
+        trayIcon = new TrayIcon(image, name);
+        //Let the system resizes the image if needed
+        trayIcon.setImageAutoSize(true);
+        //Set tooltip text for the tray icon
+        trayIcon.setToolTip(tooltip);
+        try {
+			tray.add(trayIcon);
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+    public static void displayNotif(String title, String desc, MessageType type) {
         if (SystemTray.isSupported()) {
-            Notification td = new Notification();
-            td.displayTray();
+        	trayIcon.displayMessage(title, desc, type);
         } else {
             System.err.println("System tray not supported!");
         }
     }
-
-    public void displayTray() throws AWTException, java.net.MalformedURLException {
-        //Obtain only one instance of the SystemTray object
-        SystemTray tray = SystemTray.getSystemTray();
-
-        //If the icon is a file
-        Image image = Toolkit.getDefaultToolkit().createImage("icon.png");
-        //Alternative (if the icon is on the classpath):
-        //Image image = Toolkit.getToolkit().createImage(getClass().getResource("icon.png"));
-        TrayIcon trayIcon = new TrayIcon(image, "Tray Demo");
-        //Let the system resizes the image if needed
-        trayIcon.setImageAutoSize(true);
-        //Set tooltip text for the tray icon
-        trayIcon.setToolTip("System tray icon demo");
-        tray.add(trayIcon);
-        trayIcon.displayMessage("Hello, World", "notification demo", MessageType.INFO);
-    }
+    public static void main(String[] args) {
+		init("Test","A thing","textures/icon.png");
+		
+		displayNotif("YOOOOOO", "Thats KEWL", MessageType.INFO);
+	}
 }
