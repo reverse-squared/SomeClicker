ce9a70ec3cc42b3859c5e2da3a0924b176e31974
hparcells <hunterparcells@gmail.com>
2017-09-28 15:46:41 -0400

    Added More Stuff for ImDaveead

--- src/com/wearedevs/javaclicker/handlers/CaseHandler.java
+++ src/com/wearedevs/javaclicker/handlers/CaseHandler.java
@@ -1,5 +1,14 @@
 package com.wearedevs.javaclicker.handlers;
 
-public class CaseHandler {
-
+public abstract class CaseHandler {
+	public boolean unlocked;
+	
+	public CaseHandler() {
+		//Do Stuff
+	}
+	
+	public abstract void openCase();
+	public abstract void getItem();
+	
+	public abstract String getName();
 }
