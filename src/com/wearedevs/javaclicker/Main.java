ef633ba9e8a7caf270705e96a0efa99f95fba136
EXPGamer303 <expgamer303@gmail.com>
2017-09-28 21:17:55 -0400

    DEV 0.5 Release

--- src/com/wearedevs/javaclicker/Main.java
+++ src/com/wearedevs/javaclicker/Main.java
@@ -22,7 +22,7 @@ public class Main extends JFrame {
 	public static MainPanel mainPanel;
 	public static ShopPanel shopPanel;
 	
-	public static final String VERSION = "DEV 0.4";
+	public static final String VERSION = "DEV 0.5";
 	
 	public static final Rectangle windowSize = new Rectangle(100, 100, 640, 480);
 	public static final Rectangle panelSize = new Rectangle(0, 0, windowSize.width, windowSize.height);
