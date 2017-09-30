2403796f30c0974cc64c963cbf93a8380b30b923
hparcells <hunterparcells@gmail.com>
2017-09-30 08:46:31 -0400

    Added a CasePanel

---
+++ src/com/wearedevs/javaclicker/gui/CasePanel.java
@@ -0,0 +1,16 @@
+package com.wearedevs.javaclicker.gui;
+
+import javax.swing.JPanel;
+
+import com.wearedevs.javaclicker.Main;
+
+public class CasePanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public CasePanel() {
+		setLayout(null);
+		setBounds(Main.windowSize);
+	}
+
+}
