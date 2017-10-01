65d06a3af76946fe8be3f98416ae8236d311f568
EXPGamer303 <expgamer303@gmail.com>
2017-10-01 15:32:57 -0400

    Added Cases Button

--- src/com/wearedevs/javaclicker/gui/CasePanel.java
+++ src/com/wearedevs/javaclicker/gui/CasePanel.java
@@ -1,16 +1,30 @@
 package com.wearedevs.javaclicker.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.wearedevs.javaclicker.Main;
 
 public class CasePanel extends JPanel {
 
-	private static final long serialVersionUID = 1L;
-
 	public CasePanel() {
 		setLayout(null);
 		setBounds(Main.windowSize);
+		
+		JButton backBtn = new JButton("Go Back");
+		backBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Main.updateCounter();
+				
+				Main.main.setContentPane(Main.mainPanel);
+			}
+		});
+		backBtn.setBounds(12, 398, 270, 40);
+		add(backBtn);
 	}
 
 }
