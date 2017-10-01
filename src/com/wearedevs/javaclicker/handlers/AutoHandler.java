f63daf06c7d86814a310cfcaf67f6e7ba2ad3c1a
ImDaveead <imdaveead@gmail.com>
2017-10-01 19:04:44 -0400

    made a game loop instead of sleep

--- src/com/wearedevs/javaclicker/handlers/AutoHandler.java
+++ src/com/wearedevs/javaclicker/handlers/AutoHandler.java
@@ -1,7 +1,6 @@
 package com.wearedevs.javaclicker.handlers;
 
-import com.wearedevs.javaclicker.Main;
-import com.wearedevs.javaclicker.util.RandomUtil;
+import utils.GameLoop;
 
 /**
  * Handler for auto clicking related things
@@ -9,7 +8,7 @@ import com.wearedevs.javaclicker.util.RandomUtil;
  * @author ImDaveead
  */
 public class AutoHandler {
-	public static Thread autoThread;
+	public static GameLoop game_loop;
 	
 	public static int autoClick = 0;
 	public static int clickBomb = 0;
@@ -19,6 +18,9 @@ public class AutoHandler {
 	
 	public static boolean autoClickStarted = false;
 	
+	public static int ticks = 0;
+	public static double seconds = 0; //large limit :)
+	
 	public static void initAutoThread() {
 		if(autoClickStarted) {
 			return;
@@ -26,60 +28,14 @@ public class AutoHandler {
 		
 		autoClickStarted = true;
 		
-		autoThread = new Thread(new Runnable() {
+		game_loop = new GameLoop(60,new Runnable() {
+
 			public void run() {
-				int bomb_count = 0;
-				int factory_count = 0;
-				int mine_count = 0;
-				int portal_count = 0;
-				
-				try {
-					while (autoClickStarted) {
-						bomb_count++;
-						factory_count++;
-						mine_count++;
-						portal_count++;
-						
-						//Bomb Cycle
-						if (bomb_count > 100) {
-							bomb_count = 0;
-							double bomb = (clickBomb + RandomUtil.randomRange(clickBomb * -0.15, clickBomb * 0.15));
-							Main.clicks += bomb;
-						}
-						
-						//Factory Cycle
-						if(factory_count > 50) {
-							factory_count = 0;
-							
-							Main.clicks += factory;
-						}
-						
-						//Mine Cycle
-						if(mine_count > 10) {
-							mine_count = 0;
-							
-							Main.clicks += mine;
-						}
-						
-						//Portal Cycle
-						if(portal_count > 150) {
-							portal_count = 0;
-							
-							Main.clicks += portal;
-						}
-						Main.clicks += autoClick / 10d;
-						Main.updateCounter();
-						
-						//TODO: Better Timer
-						Thread.sleep(100L);
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+				ticks++;
+				if(ticks>=60) {
+					seconds++;
 				}
 			}
-			
 		});
-		
-		autoThread.start();
 	}
 }
