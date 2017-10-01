ed5a6eb5aad9553fb81ccc3b56120f3b8def48c8
EXPGamer303 <expgamer303@gmail.com>
2017-10-01 15:34:47 -0400

    Updated Case Button

--- src/com/wearedevs/javaclicker/gui/CasePanel.java
+++ src/com/wearedevs/javaclicker/gui/CasePanel.java
@@ -1,10 +1,13 @@
 package com.wearedevs.javaclicker.gui;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import com.wearedevs.javaclicker.Main;
 
@@ -12,7 +15,13 @@ public class CasePanel extends JPanel {
 
 	public CasePanel() {
 		setLayout(null);
-		setBounds(Main.windowSize);
+		setBounds(Main.panelSize);
+		
+		JLabel labelTitle = new JLabel("Cases");
+		labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitle.setFont(new Font("Tahoma", Font.BOLD, 36));
+		labelTitle.setBounds(12, 13, 616, 67);
+		add(labelTitle);
 		
 		JButton backBtn = new JButton("Go Back");
 		backBtn.addActionListener(new ActionListener() {
