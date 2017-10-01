61e9c84029ad30b68629d93f3e1be14d03463ff9
EXPGamer303 <expgamer303@gmail.com>
2017-10-01 19:59:37 -0400

    Moved Things and Added a Blackhole

---
+++ src/com/wearedevs/javaclicker/shop/items/reactor/reactor/Reactor1.java
@@ -0,0 +1,23 @@
+package com.wearedevs.javaclicker.shop.items.reactor.reactor;
+
+import com.wearedevs.javaclicker.handlers.AutoHandler;
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class Reactor1 extends ShopItem {
+
+	public void onPurchase() {
+		AutoHandler.reactorGen = 10000;
+		AutoHandler.reactorSuck = 10; // 6000 sucked
+		
+		AutoHandler.initAutoThread();
+	}
+
+	public int getPrice() {
+		return 100000;
+	}
+
+	public String getName() {
+		return "Reactor: Tier 1";
+	}
+
+}
