5ac45644171c6b76af780e3e72fb1720b077e47a
hparcells <hunterparcells@gmail.com>
2017-10-02 18:08:42 -0400

    Added a Stupid Javadoc for AutoMAX

--- src/com/wearedevs/javaclicker/shop/items/autoClickers/AutoMAX.java
+++ src/com/wearedevs/javaclicker/shop/items/autoClickers/AutoMAX.java
@@ -3,6 +3,11 @@ package com.wearedevs.javaclicker.shop.items.autoClickers;
 import com.wearedevs.javaclicker.handlers.AutoHandler;
 import com.wearedevs.javaclicker.shop.ShopItem;
 
+/**
+ * A stupid upgrade made by ImDaveead that should never exist. This autoclicker is the best autoclicker you can get since 
+ * we use {@code Integer.MAX_VALUE} for clicks per second. The way we store the clicks in in a double so we don't run into
+ * a problem later on about the user's amount of clicks being the integer limit, and not being able to increment it anymore.
+ */
 public class AutoMAX extends ShopItem {
 
 	public void onPurchase() {
