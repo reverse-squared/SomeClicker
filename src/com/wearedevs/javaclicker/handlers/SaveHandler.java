69e9c88ff1d7bdc77c440a1b9c9e3737b34b5f47
hparcells <hunterparcells@gmail.com>
2017-10-02 18:57:49 -0400

    Tried to Do Load Stuff

--- src/com/wearedevs/javaclicker/handlers/SaveHandler.java
+++ src/com/wearedevs/javaclicker/handlers/SaveHandler.java
@@ -1,8 +1,14 @@
 package com.wearedevs.javaclicker.handlers;
 
+import java.awt.List;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import com.wearedevs.javaclicker.Main;
 
 /**
@@ -10,8 +16,11 @@ import com.wearedevs.javaclicker.Main;
  *
  */
 public class SaveHandler {
+	static ArrayList<Integer> list = new ArrayList<Integer>();
+	
 	static File file = new File("clicks.txt");
 	static boolean exists = file.exists();
+	static BufferedReader reader = null;
 	
 	/**
 	 * Saves The Game
@@ -30,8 +39,27 @@ public class SaveHandler {
 	 * Loads The Game
 	 */
 	public static void load() {
-		if(exists) {
-			//TODO: Read clicks.txt
+		if(exists) {			
+			try {
+			    try {
+					reader = new BufferedReader(new FileReader(file));
+				} catch (FileNotFoundException e1) {
+					e1.printStackTrace();
+				}
+			    String text = null;
+
+			    try {
+					while ((text = reader.readLine()) != null) {
+					    list.add(Integer.parseInt(text));
+					}
+				} catch (NumberFormatException | IOException e) {
+					e.printStackTrace();
+				}
+			    
+				Main.clicks = text;
+			}finally {
+				
+			}
 		}else {
 			save();
 		}
