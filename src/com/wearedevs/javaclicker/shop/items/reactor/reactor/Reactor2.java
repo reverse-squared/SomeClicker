a23febc06fa9ba19a643044f5371e6ee7ae56e37
hparcells <hunterparcells@gmail.com>
2017-10-01 20:05:26 -0400

    Added More Reactors

--- src/com/wearedevs/javaclicker/shop/items/reactor/reactor/Reactor2.java
+++ src/com/wearedevs/javaclicker/shop/items/reactor/reactor/Reactor2.java
@@ -3,19 +3,21 @@ package com.wearedevs.javaclicker.shop.items.reactor.reactor;
 import com.wearedevs.javaclicker.handlers.AutoHandler;
 import com.wearedevs.javaclicker.shop.ShopItem;
 
-public class Reactor1 extends ShopItem {
+public class Reactor2 extends ShopItem {
 
 	public void onPurchase() {
+		//TODO: Fix This
 		AutoHandler.reactorGen = 10000;
 		AutoHandler.reactorSuck = 10; // 6000 sucked
 	}
 
 	public int getPrice() {
+		//TODO: Fix Prices
 		return 100000;
 	}
 
 	public String getName() {
-		return "Reactor: Tier 1";
+		return "Reactor: Tier 2";
 	}
 
 }
