c4ca9c9ebf2f57442e5b0ca441f3ea5f8c0c0a63
ImDaveead <imdaveead@gmail.com>
2017-10-01 20:20:37 -0400

    upgrades

---
+++ src/com/wearedevs/javaclicker/shop/items/reactor/blackhole/supermassiveblackhole/SupermassiveBlackhole7.java
@@ -0,0 +1,26 @@
+package com.wearedevs.javaclicker.shop.items.reactor.blackhole.supermassiveblackhole;
+
+import com.wearedevs.javaclicker.handlers.AutoHandler;
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class SupermassiveBlackhole7 extends ShopItem {
+
+	public void onPurchase() {
+		//TODO: Fix This
+		AutoHandler.reactorGen = 10000;
+		AutoHandler.reactorSuck = 10; // 6000 sucked
+		
+		//Init AutoClicker
+		AutoHandler.initAutoThread();
+	}
+
+	public int getPrice() {
+		//TODO: Fix Prices
+		return 100000;
+	}
+
+	public String getName() {
+		return "Blackhole: Tier 1";
+	}
+
+}
