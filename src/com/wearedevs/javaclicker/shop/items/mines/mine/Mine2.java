d92a6f1ea5ef5f05bd1182cc1d0bdf5edc608e56
EXPGamer303 <expgamer303@gmail.com>
2017-10-01 20:16:54 -0400

    Added More Mines

--- src/com/wearedevs/javaclicker/shop/items/mines/mine/Mine2.java
+++ src/com/wearedevs/javaclicker/shop/items/mines/mine/Mine2.java
@@ -1,23 +1,23 @@
-package com.wearedevs.javaclicker.shop.items.mines;
+package com.wearedevs.javaclicker.shop.items.mines.mine;
 
 import com.wearedevs.javaclicker.handlers.AutoHandler;
 import com.wearedevs.javaclicker.handlers.ShopHandler;
 import com.wearedevs.javaclicker.shop.ShopItem;
 
-public class Mine4 extends ShopItem{
+public class Mine2 extends ShopItem{
 	
 	public void onPurchase() {
-		AutoHandler.mine = 12;
+		AutoHandler.mine = 4;
 		
-		ShopHandler.unlock(new Mine5());
+		ShopHandler.unlock(new Mine3());
 	}
 
 	public int getPrice() {
-		return 7000;
+		return 4000;
 	}
 
 	public String getName() {
-		return "Click Mine: Tier 4";
+		return "Click Mine: Tier 2";
 	}
 
 }
