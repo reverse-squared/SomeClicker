6b1e13ce43a66334ea48a40c9d315b5ff70209d5
hparcells <hunterparcells@gmail.com>
2017-10-01 20:06:48 -0400

    Added Blackholes

---
+++ src/com/wearedevs/javaclicker/shop/items/reactor/blackhole/Blackhole3.java
@@ -0,0 +1,23 @@
+package com.wearedevs.javaclicker.shop.items.reactor.blackhole;
+
+import com.wearedevs.javaclicker.handlers.AutoHandler;
+import com.wearedevs.javaclicker.shop.ShopItem;
+
+public class Blackhole3 extends ShopItem {
+
+	public void onPurchase() {
+		//TODO: Fix This
+		AutoHandler.reactorGen = 10000;
+		AutoHandler.reactorSuck = 10; // 6000 sucked
+	}
+
+	public int getPrice() {
+		//TODO: Fix Prices
+		return 100000;
+	}
+
+	public String getName() {
+		return "Blackhole: Tier 3";
+	}
+
+}
