1076638848e11b6c8efc20e8731e4fffe962ee47
hparcells <hunterparcells@gmail.com>
2017-10-04 15:47:19 -0400

    Added caseList ArrayList

--- src/com/wearedevs/javaclicker/handlers/CaseHandler.java
+++ src/com/wearedevs/javaclicker/handlers/CaseHandler.java
@@ -1,13 +1,18 @@
 package com.wearedevs.javaclicker.handlers;
 
+import java.util.ArrayList;
+
 import com.wearedevs.javaclicker.cases.Case;
 import com.wearedevs.javaclicker.cases.CaseOutcome;
 import com.wearedevs.javaclicker.util.WeightedCollection;
 
 public class CaseHandler {
+	public static ArrayList<Case> caseList = new ArrayList<Case>();
+	
 	public static void openCase(Case c) {
 		WeightedCollection<CaseOutcome> out = c.getAllOutcomes();
-		
+		caseList.add(new String("lol"));
+		System.out.println(cases);
 		
 		//Chose the random
 		CaseOutcome chosen = out.next();
