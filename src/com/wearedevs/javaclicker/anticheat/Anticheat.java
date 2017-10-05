6fbbdf2e8599d66ca3b98bb88941ae70231bb9e7
EXPGamer303 <expgamer303@gmail.com>
2017-10-05 19:04:41 -0400

    Tried to Do Anticheat

--- src/com/wearedevs/javaclicker/anticheat/Anticheat.java
+++ src/com/wearedevs/javaclicker/anticheat/Anticheat.java
@@ -4,8 +4,39 @@ import com.wearedevs.javaclicker.Main;
 
 public class Anticheat {
 
+	private static double prevTime = 0;
+	private static double time = 0;
+	private static int suspic = 0;
+	private static boolean run = false;
+	
 	public static void checkCheats() {
 		
+		if(run) {
+			run = false;
+			
+			prevTime = time;
+			
+			if(prevTime == time) {
+				suspic++;
+				
+				if(suspic >= 10) {
+					cheater();
+				}
+			}
+			
+		}else {
+			run = true;
+			
+			addTime();
+		}
+	}
+	
+	public static void addTime() {
+		while(run) {
+			time += 0.0001;
+			
+			System.out.println(time);
+		}
 	}
 	
 	public static void cheater() {
