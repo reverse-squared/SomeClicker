ce0d5cfa7e2ab70636288c9e1a137cffb05c63b7
EXPGamer303 <expgamer303@gmail.com>
2017-10-05 15:32:13 -0400

    Added a cheaterPanel when Caught Cheating

--- src/com/wearedevs/javaclicker/anticheat/Anticheat.java
+++ src/com/wearedevs/javaclicker/anticheat/Anticheat.java
@@ -1,5 +1,14 @@
 package com.wearedevs.javaclicker.anticheat;
 
+import com.wearedevs.javaclicker.Main;
+
 public class Anticheat {
 
+	public static void checkCheats() {
+		
+	}
+	
+	public static void cheater() {
+		Main.main.setContentPane(Main.cheaterPanel);
+	}
 }
