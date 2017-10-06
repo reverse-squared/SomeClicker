0919626d02de95de4e0462f600c938ea395a771f
ImDaveead <imdaveead@gmail.com>
2017-10-05 20:30:24 -0400

    Case Open Works

--- src/com/wearedevs/javaclicker/cases/cases/ClickCase.java
+++ src/com/wearedevs/javaclicker/cases/cases/ClickCase.java
@@ -21,6 +21,17 @@ public class ClickCase extends Case {
 			}
 		});
 		
+		//750 Clicks
+		out.add(60, new CaseOutcome() {
+			public void onOutcome() {
+				Main.clicks += 750;
+			}
+			
+			public String getName() {
+				return "750 Clicks";
+			}
+		});
+		
 		//1000 Clicks
 		out.add(30, new CaseOutcome() {
 			public void onOutcome() {
@@ -31,6 +42,16 @@ public class ClickCase extends Case {
 				return "1000 Clicks";
 			}
 		});
+		//2000 Clicks
+		out.add(30, new CaseOutcome() {
+			public void onOutcome() {
+				Main.clicks += 2000;
+			}
+			
+			public String getName() {
+				return "2000 Clicks";
+			}
+		});
 		
 		//5000 Clicks
 		out.add(10, new CaseOutcome() {
@@ -39,6 +60,24 @@ public class ClickCase extends Case {
 			}
 			public String getName() {
 				return "5000 Clicks";
+			}
+		});
+		//6000 Clicks
+		out.add(10, new CaseOutcome() {
+			public void onOutcome() {
+				Main.clicks += 6000;
+			}
+			public String getName() {
+				return "6000 Clicks";
+			}
+		});
+		//4000 Clicks
+		out.add(10, new CaseOutcome() {
+			public void onOutcome() {
+				Main.clicks += 4000;
+			}
+			public String getName() {
+				return "4000 Clicks";
 			}
 		});
 		
--- src/com/wearedevs/javaclicker/handlers/CaseHandler.java
+++ src/com/wearedevs/javaclicker/handlers/CaseHandler.java
@@ -6,11 +6,14 @@ import com.wearedevs.javaclicker.Main;
 import com.wearedevs.javaclicker.cases.Case;
 import com.wearedevs.javaclicker.cases.CaseOutcome;
 import com.wearedevs.javaclicker.gui.CaseOpenPanel;
+import com.wearedevs.javaclicker.util.PlaySound;
 import com.wearedevs.javaclicker.util.WeightedCollection;
 
 public class CaseHandler {
 	public static ArrayList<Case> caseList = new ArrayList<Case>();
 	public static int caseSpd;
+	public static int caseDelay;
+	public static int caseDelay2;
 	
 	/**
 	 * extra c because {@code case} keyword
@@ -21,7 +24,9 @@ public class CaseHandler {
 	public static void openCase(Case c) {
 		ccase = c;
 		caseOutcomes = c.getAllOutcomes();
-		caseSpd = 300;
+		caseSpd = 0;
+		caseDelay = 100;
+		caseDelay2 = 0;
 		
 		Main.caseOpenPanel = new CaseOpenPanel(c);
 		Main.main.setContentPane(Main.caseOpenPanel);
@@ -31,13 +36,28 @@ public class CaseHandler {
 	 * Used By {@code AutoHandler}
 	 */
 	public static void tickCase(Case caseOpening) {
-		caseSpd--;
-
-		if (caseSpd <= 0) {
-			//Open the case
+		if(caseDelay>0) {
+			caseDelay--;
+		}
+		caseDelay2--;
+		if(caseDelay2<=0) {
+			//OPEN A CASE
+			CaseOutcome open = caseOutcomes.next();
+			if(caseDelay<=0) {
+				caseSpd++;
+				System.out.println(caseSpd);
+				if(caseSpd>25) {
+					//Stop
+					open.onOutcome();
+					ccase = null;
+					Main.caseOpenPanel.btnGoBack.setVisible(true);
+					PlaySound.playSound("/sound/buy.wav");
+				}
+			}
 			
-			ccase = null;
-			return;
+			caseDelay2 = caseSpd;
+			//TODO: Sound
+			Main.caseOpenPanel.label_opening.setText(open.getName());
 		}
 		
 	}
