a8e5a6fd565097a0fb85c0ada39eeb753c7cbd71
hparcells <hunterparcells@gmail.com>
2017-10-05 20:46:49 -0400

    Added Case Sounds

--- src/com/wearedevs/javaclicker/Main.java
+++ src/com/wearedevs/javaclicker/Main.java
@@ -55,7 +55,9 @@ public class Main extends JFrame {
 			public void run() {
 				try {
 					main = new Main();
-					CaseHandler.openCase(new ClickMultiplierCase());
+					
+					//Dev Code
+					CaseHandler.openCase(new ClickCase());
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
