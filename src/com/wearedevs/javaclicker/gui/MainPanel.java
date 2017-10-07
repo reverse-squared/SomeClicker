0aee86e65f85b5babd4f0021f1d6929c5de985e2
EXPGamer303 <expgamer303@gmail.com>
2017-10-07 15:35:59 -0400

    Improvements

--- src/com/wearedevs/javaclicker/gui/MainPanel.java
+++ src/com/wearedevs/javaclicker/gui/MainPanel.java
@@ -96,6 +96,16 @@ public class MainPanel extends JPanel {
 		toggleReactor.setBounds(12, 170, 270, 40);
 		add(toggleReactor);
 		
+		JButton modsButton = new JButton("Mods");
+		modsButton.setFont(new Font("Tahoma", Font.BOLD, 17));
+		modsButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main.main.setContentPane(Main.modPanel);
+			}
+		});
+		modsButton.setBounds(12, 300, 270, 40);
+		add(modsButton);
+		
 		JButton devButton = new JButton("Dev Button");
 		devButton.setFont(new Font("Tahoma", Font.BOLD, 17));
 		devButton.addActionListener(new ActionListener() {
--- src/com/wearedevs/javaclicker/mod/Mod.java
+++ src/com/wearedevs/javaclicker/mod/Mod.java
@@ -1,5 +1,8 @@
 package com.wearedevs.javaclicker.mod;
 
+/**
+ * Mods
+ */
 public class Mod {
 	
 	/**
