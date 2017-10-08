3987f44f4a58f3adf84a0efd20a81d4f4a2a76dc
hparcells <hunterparcells@gmail.com>
2017-10-08 17:48:48 -0400

    Added More Autoclickers for Late Game Balance

--- src/com/wearedevs/javaclicker/shop/items/autoClickers/Auto9.java
+++ src/com/wearedevs/javaclicker/shop/items/autoClickers/Auto9.java
@@ -1,23 +1,20 @@
 package com.wearedevs.javaclicker.shop.items.autoClickers;
 
 import com.wearedevs.javaclicker.handlers.AutoHandler;
-import com.wearedevs.javaclicker.handlers.ShopHandler;
 import com.wearedevs.javaclicker.shop.ShopItem;
 
-public class Auto5 extends ShopItem {
+public class Auto9 extends ShopItem {
 
 	public void onPurchase() {
-		AutoHandler.autoClick = 30;
-		
-		ShopHandler.unlock(new Auto6());
+		AutoHandler.autoClick = 300;
 	}
 
 	public int getPrice() {
-		return 10000;
+		return 123000;
 	}
 
 	public String getName() {
-		return "Auto Clicker: Tier 5";
+		return "Auto Clicker: Tier 9";
 	}
 
 }
