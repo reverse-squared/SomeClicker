fc9e83e950e72e8a912b4edcfd42c0d542950c48
hparcells <hunterparcells@gmail.com>
2017-10-09 17:03:43 -0400

    Added a Button to The Extras Panel

--- src/com/wearedevs/javaclicker/Main.java
+++ src/com/wearedevs/javaclicker/Main.java
@@ -64,9 +64,11 @@ public class Main extends JFrame {
 	public static boolean resetOnClose = false;
 	
 	public static final String path = System.getenv("APPDATA") + "/WeAreDevs/JavaClicker/";
+	
 	public static final String modPath = System.getenv("APPDATA") + "/WeAreDevs/JavaClicker/mods/";
-	public static final File modFile = new File(System.getenv("APPDATA") + "/WeAreDevs/JavaClicker/mods/modlist.txt");
 	public static final String lockFileLoc = System.getenv("APPDATA") + "/WeAreDevs/JavaClicker/startup.lock";
+	
+	public static final File modFile = new File(System.getenv("APPDATA") + "/WeAreDevs/JavaClicker/mods/modlist.txt");
 	public static File startupLockFile = new File(lockFileLoc);
 	
 	
