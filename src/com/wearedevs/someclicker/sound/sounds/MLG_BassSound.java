7e3d1b65c8e3f7726a14a0eaef547b7ff82f4849
hparcells <hunterparcells@gmail.com>
2017-10-11 20:25:10 -0400

    lol cases

---
+++ src/com/wearedevs/someclicker/sound/sounds/MLG_BassSound.java
@@ -0,0 +1,15 @@
+package com.wearedevs.someclicker.sound.sounds;
+
+import com.wearedevs.someclicker.sound.Sound;
+
+public class MLG_BassSound extends Sound {
+
+	public String getName() {
+		return "MLG Bass";
+	}
+
+	public String getFileName() {
+		return "MLG_bass.wav";
+	}
+
+}
