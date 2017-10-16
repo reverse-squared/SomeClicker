0d33504b403646f3fc025ed7019d5c064ba13891
EXPGamer303 <expgamer303@gmail.com>
2017-10-16 18:24:15 -0400

    5.2

--- src/com/wearedevs/someclicker/Main.java
+++ src/com/wearedevs/someclicker/Main.java
@@ -58,7 +58,7 @@ public class Main extends JFrame {
 	public static CheaterPanel cheaterPanel;
 	public static ExtrasPanel extrasPanel;
 
-	public static final String VERSION = "Beta 5.1";
+	public static final String VERSION = "Beta 5.2";
 	public static final int VERSION_NUM = 2;
 
 	public static final Rectangle windowSize = new Rectangle(100, 100, 640, 480);
