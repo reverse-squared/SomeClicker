773d24a9b726d82debb56c66d41c75934fbe221a
hparcells <hunterparcells@gmail.com>
2017-10-17 16:47:26 -0400

    Sound Classes

---
+++ src/com/wearedevs/someclicker/sound/sounds/MLG_WowSound.java
@@ -0,0 +1,15 @@
+package com.wearedevs.someclicker.sound.sounds;
+
+import com.wearedevs.someclicker.sound.Sound;
+
+public class MLG_WowSound extends Sound {
+
+	public String getName() {
+		return "MLG Wow";
+	}
+
+	public String getFileName() {
+		return "MLG_wow.wav";
+	}
+
+}
